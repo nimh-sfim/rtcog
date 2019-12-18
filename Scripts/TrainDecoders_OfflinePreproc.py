14310419acb4b1817c7f4b19b48a5dada4d77bb9
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2019-12-18 12:09:38 -0500

    TrainDecoders_Offline now uses config file

--- Scripts/TrainDecoders_OfflinePreproc.py
+++ Scripts/TrainDecoders_OfflinePreproc.py
@@ -13,13 +13,6 @@
 #     name: psychopy
 # ---
 
-TRAIN_Path = '/data/SFIMJGC_HCP7T/PRJ_rtCAPs/PrcsData/TECH07/D01_RT_Run01/pb04.TECH07_Run01_Training.nii'
-TRAIN_Motion_Path = '/data/SFIMJGC_HCP7T/PRJ_rtCAPs/PrcsData/TECH07/D01_RT_Run01/pb04.TECH07_Run01_Training.Motion.1D'
-MASK_Path = '/data/SFIMJGC_HCP7T/PRJ_rtCAPs/PrcsData/TECH07/D01_RT_Run01/GMribbon_R4Feed.nii'
-CAPs_Path = '/data/SFIMJGC_HCP7T/PRJ_rtCAPs/PrcsData/TECH07/D01_RT_Run01/Frontier2013_CAPs_R4Feed.nii'
-SVRs_Path = '/data/SFIMJGC_HCP7T/PRJ_rtCAPs/PrcsData/TECH07/D01_RT_Run01/Offline_SVRs.pkl'
-TRAIN_NVols_Discard = 0
-
 import nibabel as nib
 import numpy as np
 import pandas as pd
@@ -30,71 +23,39 @@ import hvplot.pandas
 from scipy.stats import zscore
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
+import config
 hv.extension('bokeh')
 
 # %%capture
 from tqdm.notebook import tqdm
 tqdm().pandas()
 
-# Load CAPs in same grid as functional (linearly interpolated)
-# ============================================================
-CAPs_Img  = nib.load(CAPs_Path)
-[CAPs_Nx, CAPs_Ny, CAPs_Nz, n_CAPs] = CAPs_Img.shape
-print ('+ CAPs Dimensions = %d CAPs | [%d,%d,%d]' % (n_CAPs, CAPs_Nx,CAPs_Ny,CAPs_Nz))
+from rtcap_lib import load_fMRI_file, mask_fMRI_img
 
-# +
-CAP_indexes = np.arange(n_CAPs)
-CAP_labels  = np.array(['CAP'+str(cap).zfill(2) for cap in CAP_indexes])
-#rCAP_indexes = np.array([15,25,2,4,18,28,24,11,21])
-#rCAP_labels  = np.array(['VMed','VPol','VLat','DMN','SMot','Audi','ExCn','rFPa','lFPa'])
-rCAP_indexes = np.array([25,4,18,28,24,11,21])
-rCAP_labels  = np.array(['VPol','DMN','SMot','Audi','ExCn','rFPa','lFPa'])
+from config import CAP_indexes, CAP_labels, CAPs_Path
+from config import TRAIN_Path as Data_Path
+from config import TRAIN_Motion_Path as Data_Motion_Path
+from config import Mask_Path
+from config import SVRs_Path
+n_CAPs      = len(CAP_indexes)
 
-for i,ii in enumerate(rCAP_indexes):
-    CAP_labels[ii] = rCAP_labels[i]
-# -
+# Load Data in Memory
+CAPs_Img     = load_fMRI_file(CAPs_Path)
+Mask_Img     = load_fMRI_file(Mask_Path)
+Data_Img    = load_fMRI_file(Data_Path)
+Data_Motion = np.loadtxt(Data_Motion_Path)
+print('++ CAPs Dimensions  : %s' % str(CAPs_Img.header.get_data_shape()))
+print('++ DATA Dimensions  : %s' % str(Data_Img.header.get_data_shape()))
+print('++ MASK Dimensions  : %s' % str(Mask_Img.header.get_data_shape()))
+print('++ Motion Dimensions: %s' % str(Data_Motion.shape))
 
-# Load FOV mask in same grid as functional (ensure it only cover areas that have data on input and CAPs)
-# ======================================================================================================
-MASK_Img  = nib.load(MASK_Path)
-[MASK_Nx, MASK_Ny, MASK_Nz] = MASK_Img.shape
-n_vox = MASK_Img.get_data().sum()
-print ('+ Mask Dimensions = %d Voxels in mask | [%d,%d,%d]' % (n_vox, MASK_Nx,MASK_Ny,MASK_Nz))
-print ('+ Mask Path: %s' % MASK_Path)
-
-# Load Training Dataset
-TRAIN_Img = nib.load(TRAIN_Path)
-[TRAIN_Nx, TRAIN_Ny, TRAIN_Nz, TRAIN_Nt] = TRAIN_Img.shape
-print ('+ Data Dimensions = [%d,%d,%d, Nvols = %d]' % (TRAIN_Nx, TRAIN_Ny, TRAIN_Nz, TRAIN_Nt))
-
-TRAIN_Motion = np.loadtxt(TRAIN_Motion_Path)
-print(TRAIN_Motion.shape)
-
-# ***
-# ## Vectorize all analysis inputs (Space is lost)
-
-# +
-# Vectorize All
-# =============
-MASK_Vector  = np.reshape(MASK_Img.get_data(),(MASK_Nx*MASK_Ny*MASK_Nz),          order='F')
-CAPs_Vector  = np.reshape(CAPs_Img.get_data(),(CAPs_Nx*CAPs_Ny*CAPs_Nz, n_CAPs),  order='F')
-TRAIN_Vector = np.reshape(TRAIN_Img.get_data(),(TRAIN_Nx*TRAIN_Ny*TRAIN_Nz, TRAIN_Nt), order='F')
-
-# Minimize rounding errors
-# ========================
-CAPs_Vector  = CAPs_Vector.astype('float64')
-TRAIN_Vector = TRAIN_Vector.astype('float64')
-
-# Mask All (and keep only CAPs of interest)
-# =========================================
-CAPs_InMask = CAPs_Vector[MASK_Vector==1,:]
-CAPs_InMask = CAPs_InMask[:,CAP_indexes]
-TRAIN_InMask = TRAIN_Vector[MASK_Vector==1,:]
-TRAIN_InMask = TRAIN_InMask[:,np.arange(TRAIN_NVols_Discard,TRAIN_Nt)] # Discard Volumes not yet stable (takes some time for on-line operations to stabilize)
-[TRAIN_InMask_Nv, TRAIN_InMask_Nt] = TRAIN_InMask.shape
-print('++ Final CAPs Template Dimensions: %d CAPs with %d voxels' % (CAPs_InMask.shape[1],CAPs_InMask.shape[0]))
-print('++ Final Training Data Dimensions: %d Acqs with %d voxels' % (TRAIN_InMask_Nt, TRAIN_InMask_Nv))
-# -
+# Mask & Vectorize Data
+CAPs_InMask = mask_fMRI_img(CAPs_Img,Mask_Img)
+CAPs_InMask = CAPs_InMask[:, CAP_indexes]                            # Select only CAPs of intrest 
+Data_InMask = mask_fMRI_img(Data_Img,Mask_Img)
+[Data_Nv, Data_Nt] = Data_InMask.shape
+print('++ Masked CAPs Dimensions  : %s' % str(CAPs_InMask.shape))
+print('++ Masked Data Dimensions  : %s' % str(Data_InMask.shape))
 
 # ***
 # # Data Standarization in Space
@@ -106,68 +67,42 @@ CAPs_InMask = sc_CAPs.fit_transform(CAPs_InMask)
 
 # Standarize Time series in Space
 # ===============================
-sc_TRAIN_space     = StandardScaler(with_mean=True, with_std=True)
-TRAIN_InMask  = sc_TRAIN_space.fit_transform(TRAIN_InMask)
+sc_Data_space = StandardScaler(with_mean=True, with_std=True)
+Data_InMask   = sc_Data_space.fit_transform(Data_InMask)
 
 # ***
 # # Generation of Traning Labels (via Linear Regression + Z-scoring)
 
 # Initialize a Dictionary for results
 Results = {}
-for cap in rCAP_labels:
+for cap in CAP_labels:
     Results[cap]=[]
 Results['R2'] = []
 
-X_fmri = pd.DataFrame(CAPs_InMask[:,rCAP_indexes], columns=rCAP_labels)
-for vol in tqdm(np.arange(TRAIN_InMask_Nt)):
-    Y_fmri = pd.Series(TRAIN_InMask[:,vol],name='V'+str(vol).zfill(4))
-    lm     = linear_model.LinearRegression()
-    model  = lm.fit(X_fmri,Y_fmri)
-    for i,cap in enumerate(rCAP_labels):
-        Results[cap].append(lm.coef_[i])
-    Results['R2'].append(lm.score(X_fmri,Y_fmri))
-LinReg_Results       = pd.DataFrame(Results)
-LinReg_Results['TR'] = np.arange(TRAIN_InMask_Nt)
-
-linear_regression_layout = (hv.Curve(LinReg_Results, kdims=['TR'],vdims=['R2'], label='R2 for Linear Regression on Training Data').opts(width=1500) + \
-LinReg_Results[rCAP_labels].hvplot(legend='top', label='Regression Coefficients for all CAPs').opts(width=1500)).cols(1)
-hv.save(linear_regression_layout,'./LinearRegression_Results.png',backend='matplotlib')
-linear_regression_layout
-
 X_fmri = pd.DataFrame(CAPs_InMask, columns=CAP_labels)
-for vol in tqdm(np.arange(TRAIN_InMask_Nt)):
-    Y_fmri = pd.Series(TRAIN_InMask[:,vol],name='V'+str(vol).zfill(4))
+for vol in tqdm(np.arange(Data_Nt)):
+    Y_fmri = pd.Series(Data_InMask[:,vol],name='V'+str(vol).zfill(4))
     lm     = linear_model.LinearRegression()
     model  = lm.fit(X_fmri,Y_fmri)
     for i,cap in enumerate(CAP_labels):
         Results[cap].append(lm.coef_[i])
     Results['R2'].append(lm.score(X_fmri,Y_fmri))
 LinReg_Results       = pd.DataFrame(Results)
-LinReg_Results['TR'] = np.arange(TRAIN_InMask_Nt)
+LinReg_Results['TR'] = np.arange(Data_Nt)
 
 linear_regression_layout = (hv.Curve(LinReg_Results, kdims=['TR'],vdims=['R2'], label='R2 for Linear Regression on Training Data').opts(width=1500) + \
-LinReg_Results[rCAP_labels].hvplot(legend='top', label='Regression Coefficients for all CAPs').opts(width=1500)).cols(1)
+LinReg_Results[CAP_labels].hvplot(legend='top', label='Regression Coefficients for all CAPs').opts(width=1500)).cols(1)
 hv.save(linear_regression_layout,'./LinearRegression_Results.png',backend='matplotlib')
 linear_regression_layout
-
-# + active=""
-# # Steve LaConte uses the Z-score version of the regression coefficients as the labels
-# # ===================================================================================
-# LinReg_Zscores = pd.DataFrame(columns=LinReg_Results.columns, index=LinReg_Results.index)
-# for col in LinReg_Results.columns:
-#     LinReg_Zscores[col] = zscore(LinReg_Results[col])
-# LinReg_Zscores['TR'] = LinReg_Results['TR']
-# LinReg_Zscores.drop(['TR','R2'], axis=1).hvplot(legend='top', label='Z-scored training labels').opts(width=1500)
-# -
 
 # Becuase we don't want to make all CAPs equally plausible, I believe it makes sense to Z-score across all of them, and not on a CAP-by-CAP basis
 # which happened to be my original approach based on LaConte's work (but his work only had one classifier)
 # ================================================================================================================================================
-All_LinReg = LinReg_Results[rCAP_labels]
-All_LinReg = All_LinReg.values.reshape(len(rCAP_labels)*TRAIN_InMask_Nt, order='F')
+All_LinReg = LinReg_Results[CAP_labels]
+All_LinReg = All_LinReg.values.reshape(len(CAP_labels)*Data_Nt, order='F')
 All_LinReg_Z = zscore(All_LinReg)
-All_LinReg_Z = All_LinReg_Z.reshape((TRAIN_InMask_Nt,len(rCAP_labels)), order='F')
-LinReg_Zscores = pd.DataFrame(All_LinReg_Z, columns=rCAP_labels, index=LinReg_Results.index)
+All_LinReg_Z = All_LinReg_Z.reshape((Data_Nt,len(CAP_labels)), order='F')
+LinReg_Zscores = pd.DataFrame(All_LinReg_Z, columns=CAP_labels, index=LinReg_Results.index)
 LinReg_Zscores['TR'] = LinReg_Results['TR']
 LinReg_Zscores.drop(['TR'], axis=1).hvplot(legend='top', label='Z-scored training labels').opts(width=1500)
 
@@ -177,13 +112,19 @@ LinReg_Zscores.drop(['TR'], axis=1).hvplot(legend='top', label='Z-scored trainin
 C       = 1.0
 epsilon = 0.01
 SVRs    = {}
-for cap_lab in tqdm(rCAP_labels):
+for cap_lab in tqdm(CAP_labels):
     Training_Labels = LinReg_Zscores[cap_lab]
     mySVR = SVR(kernel='linear', C=C, epsilon=epsilon)
-    mySVR.fit(TRAIN_InMask.T,Training_Labels)
+    mySVR.fit(Data_InMask.T,Training_Labels)
     SVRs[cap_lab] = mySVR
 
 import pickle
 pickle_out = open(SVRs_Path,"wb")
 pickle.dump(SVRs, pickle_out)
 pickle_out.close()
+
+# +
+# load_fMRI_file?
+# -
+
+
