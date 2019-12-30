dccf9daf532cb14382ed412489c36ebb47a31f1f
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2019-12-30 16:06:21 +0100

    Options to generate GM ribbon without Cerebellum

--- Scripts/Realtime_Scanner/01_BringROIsToSubjectSpace.sh
+++ Scripts/Realtime_Scanner/01_BringROIsToSubjectSpace.sh
@@ -1,22 +1,24 @@
 set -e
 if [ $# -lt 3 ]; then
- echo -e "\x1B[0;34mUsage: $basename $0 EPIREFFILE ANATREF ROIFILE [MNITEMPLATE]\x1B[0m"
+ echo -e "\x1B[0;34mUsage: $basename $0 EPIREFFILE ANATREF CAPS_FILE [MNITEMPLATE]\x1B[0m"
  echo -e "\x1B[0;34mUsage: $basename $0 asd015_epi_run1+orig asd015_anat+orig roi11+tlrc []\x1B[0m"
  exit
 fi
 EPIREF=$1
 ANATREF=$2
-ROIFILE=$3
+CAPS_FILE=$3
+GM_TYPE=Segsy_NoCer # Segsy, Segsy_NoCer, MNI
 if [ $# -eq 4 ]; then
 	MNITEMPLATE=$4
 else
 	AFNIHOME=`which afni | awk -F / '{$NF=""}1' | tr -s ' ' '/'`
 	MNITEMPLATE=`echo ${AFNIHOME}MNI152_2009_template.nii.gz`
         MNITEMPLATE_SSW=`echo ${AFNIHOME}MNI152_2009_template_SSW.nii.gz`
-        MNITEMPLATE_GM=/data/SFIMJGC/PRJ_rtCAPs/Others/MNI152_2009_template_GM.nii.gz
-        DEFAULT_TEMPLATE=1
-
-        3dcalc -overwrite -a ${MNITEMPLATE_SSW}'[4]' -expr 'a' -prefix ${MNITEMPLATE_GM}
+        if [[ "${GM_TYPE}" == "MNI" ]]; then        
+           echo "++ INFO: MNI GM Mode selected"
+           MNITEMPLATE_GM=/data/SFIMJGC/PRJ_rtCAPs/Others/MNI152_2009_template_GM.nii.gz
+           3dcalc -overwrite -a ${MNITEMPLATE_SSW}'[4]' -expr 'a' -prefix ${MNITEMPLATE_GM}
+        fi
 fi
 EPI_NAME=`echo "${EPIREF}" | awk -F '+' '{print $1}'`
 SBJ=`echo "${EPI_NAME}" | awk -F '_' '{print $1}'`
@@ -86,7 +88,7 @@ echo "========================================================="
 if [ -d Segsy ]; then rm -rf Segsy; fi
 3dSeg -anat ANAT.bc.ns+orig -mask ANAT.mask+orig
 3dcopy -overwrite Segsy/AnatUB+orig. ANAT.bc+orig
-3dcalc -overwrite -a ${ANATREF} -b ANAT.bc+orig -m ANAT.mask+orig -expr 'm*(b*step(b))' -prefix ANAT.bc.ns
+3dcalc -overwrite -b ANAT.bc+orig -m ANAT.mask+orig -expr 'm*(b*step(b))' -prefix ANAT.bc.ns
 
 # (6) Compute transformation from MP-RAGE space into MNI space and vice-versa
 # -----------------------------------------------------------------------
@@ -141,37 +143,48 @@ cat_matvec -ONELINE MNI2REF.Xaff12.1D -I >  REF2MNI.Xaff12.1D
 echo ""
 echo "++ Bring ROI to EPI space..."
 echo "============================"
-ROIFILEPREFIX=`echo ${ROIFILE} | awk -F'+' '{print $1}' | awk -F'.' '{print $1}'` 
+CAPS_FILEPREFIX=`echo ${CAPS_FILE} | awk -F'+' '{print $1}' | awk -F'.' '{print $1}'` 
 3dAllineate -final linear -overwrite \
-            -input          ${ROIFILE}         \
+            -input          ${CAPS_FILE}         \
             -1Dmatrix_apply MNI2REF.Xaff12.1D  \
             -master         EPIREF+orig        \
-            -prefix         ${ROIFILEPREFIX}_R4Feed.nii
+            -prefix         ${CAPS_FILEPREFIX}_R4Feed.nii
 
-if [ ${DEFAULT_TEMPLATE} -gt 0 ]; then
+if [[ "${GM_TYPE}" == "MNI" ]]; then
   3dAllineate -final NN -overwrite \
               -input ${MNITEMPLATE_GM} \
               -1Dmatrix_apply MNI2REF.Xaff12.1D \
               -master EPIREF.mask.nii \
               -prefix GMribbon_R4Feed.nii
 fi
+if [[ "${GM_TYPE}" == "Segsy" || "${GM_TYPE}" == "Segsy_NoCer" ]]; then
+   3dcalc -a ./Segsy/Classes+orig. -expr 'equals(a,2)' -prefix rm.GMribbon_R4Feed.nii
+   3dmask_tool -input rm.GMribbon_R4Feed.nii -prefix rm.GMribbon_R4Feed.nii -overwrite -dilate_inputs 2 -2
+   3dAllineate -final NN -input rm.GMribbon_R4Feed.nii -1Dmatrix_apply Anat2REF.Xaff12.1D -master EPIREF+orig. -prefix GMribbon_R4Feed.nii
+   3dcalc -a GMribbon_R4Feed.nii -b EPIREF.mask_R4Feed.nii -c 'Frontier2013_CAPs_R4Feed.nii[0]' -expr 'a*b*step(abs(c))' -prefix GMribbon_R4Feed.nii -overwrite
+   rm rm.GMribbon_R4Feed.nii
+fi
 
-3dcalc -overwrite \
+# (10) Createing Final GM Ribbon Mask
+# ===================================
+if [[ "${GM_TYPE}" == "Segsy_NoCer" ]]; then
+ echo " + Removing Cerebellum......"
+ 3dcalc -overwrite -a ${AFNIHOME}/MNI_Glasser_HCP_v1.0.nii.gz -expr 'step(a)' -prefix MNI_GMwithoutCerebellum.nii
+ 3dAllineate -final NN -overwrite \
+             -input MNI_GMwithoutCerebellum.nii \
+             -1Dmatrix_apply MNI2REF.Xaff12.1D \
+             -master EPIREF.mask.nii \
+             -prefix MNI_GMwithoutCerebellum_alREF.nii
+  3dmask_tool -overwrite -inputs MNI_GMwithoutCerebellum_alREF.nii -dilate_inputs 6 -6 -prefix MNI_GMwithoutCerebellum_alREF.nii
+  3dmask_tool -overwrite -inputs MNI_GMwithoutCerebellum_alREF.nii -dilate_inputs 1 -prefix MNI_GMwithoutCerebellum_alREF.nii
+  3dcalc      -overwrite -a MNI_GMwithoutCerebellum_alREF.nii -b GMribbon_R4Feed.nii -expr 'a*b' -prefix GMribbon_R4Feed.nii
+  rm MNI_GMwithoutCerebellum.nii
+  rm MNI_GMwithoutCerebellum_alREF.nii
+else
+ 3dcalc -overwrite \
        -a GMribbon_R4Feed.nii \
        -b EPIREF.mask_R4Feed.nii \
-       -c ${ROIFILEPREFIX}_R4Feed.nii'[0]' \
+       -c ${CAPS_FILEPREFIX}_R4Feed.nii'[0]' \
        -expr 'a*b*step(abs(c))' \
        -prefix GMribbon_R4Feed.nii
-
-# Alternative GM
-3dcalc -a ./Segsy/Classes+orig. -expr 'equals(a,2)' -prefix rm.GMribbon_C_R4Feed.nii
-3dmask_tool -input rm.GMribbon_C_R4Feed.nii -prefix rm.GMribbon_C_R4Feed.nii -overwrite -dilate_inputs 2 -2
-3dAllineate -final NN -input rm.GMribbon_C_R4Feed.nii -1Dmatrix_apply Anat2REF.Xaff12.1D -master EPIREF+orig. -prefix GMribbon_C_R4Feed.nii
-3dcalc -a GMribbon_C_R4Feed.nii -b EPIREF.mask_R4Feed.nii -c 'Frontier2013_CAPs_R4Feed.nii[0]' -expr 'a*b*step(abs(c))' -prefix GMribbon_C_R4Feed.nii -overwrite
-rm rm.GMribbon_C_R4Feed.nii
-#3dAllineate -final NN -overwrite \
-#            -input          ${ROIFILEPREFIX}_ZSCORED.nii         \
-#            -1Dmatrix_apply MNI2REF.Xaff12.1D  \
-#            -master         EPIREF+orig        \
-#            -prefix         ${ROIFILEPREFIX}_ZSCORED_R4Feed.nii
-
+fi
