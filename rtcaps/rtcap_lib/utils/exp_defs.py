205ed828c33b36bf46bd6009485977cfa2ded58e
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2020-01-30 10:51:33 -0500

    How to generate Surface Views of CAPs in MNI space

---
+++ rtcaps/rtcap_lib/utils/exp_defs.py
@@ -0,0 +1,5 @@
+import os.path as osp
+PRJDIR      = '/data/SFIMJGC/PRJ_rtCAPs'
+CAPS_DIR    = osp.join(PRJDIR,'Others')
+CAP_indexes = [25,4,18,28,24,11,21]
+CAP_labels  = ['VPol','DMN','SMot','Audi','ExCn','rFPa','lFPa']
