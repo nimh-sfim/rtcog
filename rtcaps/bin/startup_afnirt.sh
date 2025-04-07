41501c9d0bf27745a63b7861299a3305a8500db7
Marly Rubin <marlyr@protonmail.com>
2025-04-07 10:46:13 -0400

    make startup script more flexible

--- rtcaps/bin/startup_afnirt.sh
+++ rtcaps/bin/startup_afnirt.sh
@@ -1,15 +1,26 @@
 # 03/28/2025 - Marly Rubin
 #
 # This script starts up AFNI realtime and sets all environment variables.
-# Requires REALTIME_DIR to be exported.
+# If REALTIME_DIR is not provided, it will default to the current working directory.
 # -------------------------------
 if [ -z "$REALTIME_DIR" ]; then
-    echo "Error: REALTIME_DIR is not set. Exiting..."
-    exit 1
+    echo "REALTIME_DIR is not set. Using current working directory."
+    REALTIME_DIR=$(pwd)
 fi
 
 echo "Realtime path: $REALTIME_DIR"
-cd $REALTIME_DIR || { echo "Failed to cd to REALTIME_DIR"; exit 1; }
+
+cd "$REALTIME_DIR" || { echo "Failed to cd to REALTIME_DIR"; exit 1; }
+
+if ! ls EPIREF+orig* &>/dev/null; then
+    echo "Error: EPIREF+orig file does not exist!"
+    exit 1
+fi
+
+if [ ! -f "GMribbon_R4Feed.nii" ]; then
+    echo "Error: GMribbon_R4Feed.nii file does not exist!"
+    exit 1
+fi
 
 echo "Starting AFNI RealTime..."
 
