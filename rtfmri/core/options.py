8cf0d7f2ad4f8b26034c016bd9d2bcd3bf4a1c2d
Marly Rubin <marlyr@protonmail.com>
2025-05-29 12:03:44 -0400

    fix --store_false options and add required arguments

--- rtfmri/core/options.py
+++ rtfmri/core/options.py
@@ -57,61 +57,63 @@ class Options:
           )
 
           parser_gen = parser.add_argument_group("General Options")
-          parser_gen.add_argument("-d", "--debug", action="store_true", dest="debug",  help="Enable debugging output")
-          parser_gen.add_argument("-s", "--silent",   action="store_true", dest="silent", help="Minimal text messages")
+          parser_gen.add_argument("-d", "--debug", action="store_true", dest="debug", help="Enable debugging output", default=None)
+          parser_gen.add_argument("-s", "--silent",   action="store_true", dest="silent", help="Minimal text messages", default=None)
           parser_gen.add_argument("-p", "--tcp_port", help="TCP port for incoming connections", action="store", type=int, dest='tcp_port')
-          parser_gen.add_argument("-S", "--show_data", action="store_true",help="display received data in terminal if this option is specified")
+          parser_gen.add_argument("-S", "--show_data", action="store_true",help="display received data in terminal if this option is specified", default=None)
           parser_gen.add_argument("--tr", help="Repetition time [sec]", dest="tr", action="store", type=float)
           parser_gen.add_argument("--ncores", help="Number of cores to use in the parallel processing part of the code", dest="n_cores", action="store",type=int)
           parser_gen.add_argument("--mask", help="Mask necessary for smoothing operation", dest="mask_path", action="store", type=Options.file_exists)
+          
           parser_iglm = parser.add_argument_group("Incremental GLM Options")
-          parser_iglm.add_argument("--polort", help="Order of Legengre Polynomials for iGLM",dest="iGLM_polort", default=2, action="store", type=int)
-          parser_iglm.add_argument("--no_iglm_motion", help="Do not use 6 motion parameters in iGLM", dest="iGLM_motion", action="store_false")
-          parser_iglm.add_argument("--nvols",      help="Number of expected volumes (for legendre pols only)", dest="nvols",default=500, action="store", type=int)
-          parser_iglm.add_argument("--discard",    help="Number of volumes to discard (they won't enter the iGLM step)", dest="discard", action="store", type=int)
+          parser_iglm.add_argument("--polort", help="Order of Legengre Polynomials for iGLM",dest="iGLM_polort", action="store", type=int)
+          parser_iglm.add_argument("--no_iglm_motion", help="Do not use 6 motion parameters in iGLM", dest="iGLM_motion", action="store_false", default=None)
+          parser_iglm.add_argument("--nvols", help="Number of expected volumes (for legendre pols only)", dest="nvols", action="store", type=int)
+          parser_iglm.add_argument("--discard", help="Number of volumes to discard (they won't enter the iGLM step)", dest="discard", action="store", type=int)
+
           parser_smo = parser.add_argument_group("Smoothing Options")
-          parser_smo.add_argument("--fwhm",      help="FWHM for Spatial Smoothing in [mm]", dest="FWHM", action="store", type=float)
-          parser_save   = parser.add_argument_group("Saving Options")
-          parser_save.add_argument("--out_dir",     help="Output directory", dest="out_dir", action="store", type=str, default="./")
-          parser_save.add_argument("--out_prefix",  help="Prefix for outputs", dest="out_prefix", action="store", type=str)
+          parser_smo.add_argument("--fwhm", help="FWHM for Spatial Smoothing in [mm]", dest="FWHM", action="store", type=float)
+
+          parser_save = parser.add_argument_group("Saving Options")
+          parser_save.add_argument("--out_dir", help="Output directory", dest="out_dir", action="store", type=str)
+          parser_save.add_argument("--out_prefix", help="Prefix for outputs", dest="out_prefix", action="store", type=str)
+          
           parser_exp = parser.add_argument_group('Experiment/GUI Options')
           parser_exp.add_argument("-e","--exp_type", help="Type of Experimental Run", type=str, choices=['preproc','esam', 'esam_test'])
-          parser_exp.add_argument("--no_gui", help="Do not open psychopy window. Only applies to pre-processing experiment type", action="store_true", dest='no_gui')
-          parser_exp.add_argument("--no_proc_chair", help="Hide crosshair during preprocessing run", action="store_true", dest='no_proc_chair')
-          parser_exp.add_argument("--fscreen", help="Use full screen for Experiment", action="store_true", dest="fullscreen")
+          parser_exp.add_argument("--no_gui", help="Do not open psychopy window. Only applies to pre-processing experiment type", action="store_true", dest='no_gui', default=None)
+          parser_exp.add_argument("--no_proc_chair", help="Hide crosshair during preprocessing run", action="store_true", dest='no_proc_chair', default=None)
+          parser_exp.add_argument("--fscreen", help="Use full screen for Experiment", action="store_true", dest="fullscreen", default=None)
           parser_exp.add_argument("--screen", help="Monitor to use", action="store", dest="screen",type=int)
+
           parser_dec = parser.add_argument_group('SVR/Decoding Options')
-          parser_dec.add_argument("--svr_start",  help="Volume when decoding should start. When we think iGLM is sufficient_stable", dest="dec_start_vol", action="store", type=int)
-          parser_dec.add_argument("--svr_path",   help="Path to pre-trained SVR models", dest="svr_path", action="store", type=Options.file_exists)
-          parser_dec.add_argument("--svr_zth",    help="Z-score threshold for deciding hits", dest="hit_zth", action="store", type=float)
-          parser_dec.add_argument("--svr_consec_vols",   help="Number of consecutive vols over threshold required for a hit", dest="nconsec_vols", action="store", type=int)
-          parser_dec.add_argument("--svr_win_activate", help="Activate windowing of individual volumes prior to hit estimation", dest="hit_dowin", action="store_true")
+          parser_dec.add_argument("--svr_start", help="Volume when decoding should start. When we think iGLM is sufficient_stable", dest="dec_start_vol", action="store", type=int)
+          parser_dec.add_argument("--svr_path", help="Path to pre-trained SVR models", dest="svr_path", action="store", type=Options.file_exists)
+          parser_dec.add_argument("--svr_zth", help="Z-score threshold for deciding hits", dest="hit_zth", action="store", type=float)
+          parser_dec.add_argument("--svr_consec_vols", help="Number of consecutive vols over threshold required for a hit", dest="nconsec_vols", action="store", type=int)
+          parser_dec.add_argument("--svr_win_activate", help="Activate windowing of individual volumes prior to hit estimation", dest="hit_dowin", action="store_true", default=None)
           parser_dec.add_argument("--svr_win_wl", help='Number of volumes for SVR windowing step', dest='hit_wl', type=int, action='store')
-          parser_dec.add_argument("--svr_mot_activate", help="Consider a hit if excessive motion", dest="hit_domot", action="store_true")
+          parser_dec.add_argument("--svr_mot_activate", help="Consider a hit if excessive motion", dest="hit_domot", action="store_true", default=None)
           parser_dec.add_argument("--svr_mot_th", help="Framewise Displacement Treshold for motion",  action="store", type=float, dest="svr_mot_th")
           parser_dec.add_argument("--svr_hit_mehod", help="Method for deciding hits", type=str, choices=["method01"], action="store", dest="hit_method")
           parser_dec.add_argument("--svr_vols_noqa", help="Min. number of volumes to wait since end of last QA before declaing a new hit.", type=int, dest='vols_noqa', action="store")
           parser_dec.add_argument("--q_path", help="The path to the questions json file containing the question stimuli. If not a full path, it will default to look in RESOURCES_DIR", type=str, dest='q_path', action="store")
+
           parser_dec = parser.add_argument_group('Testing Options')
-          parser_dec.add_argument("--snapshot",  help="Run snapshot test", dest="snapshot", action="store_true")
+          parser_dec.add_argument("--snapshot", help="Run snapshot test", dest="snapshot", action="store_true", default=None)
 
           cli_args = parser.parse_args(remaining_args)
 
           # Only override yaml config with CLI args that were explicitly passed
           for k, v in vars(cli_args).items():
-               if isinstance(v, bool):
-                    # Update store_true/store_false flags only if True
-                    if v:
-                         config[k] = v
-               elif v is not None:
+               if v is not None:
                     config[k] = v
           
-          required_args = ['exp_type', 'mask_path', 'nvols']
+          required_args = ['exp_type', 'mask_path', 'nvols', 'out_dir', 'out_prefix']
 
           missing = []
           for arg in required_args:
                if not arg in config or config[arg] is None:
-                    missing.append(arg) 
+                    missing.append(f'--{arg}') 
           if missing:
                print(f"++ ERROR: The following arguments are required: {', '.join(missing)}")
                sys.exit(-1)
