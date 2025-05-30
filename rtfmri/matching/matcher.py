d9d789581f122b90373008b215b7e0c7e808a5ad
Marly Rubin <marlyr@protonmail.com>
2025-05-30 17:01:38 -0400

    wip: update matcher class, refs #21

--- rtfmri/matching/matcher.py
+++ rtfmri/matching/matcher.py
@@ -11,8 +11,8 @@ log = get_logger()
 
 class Matcher:
     """Base class for matching processed TR data to given templates"""
-    def __init__(self, options):
-        self.dec_start_vol = options.dec_start_vol # First volume to do decoding on
+    def __init__(self, match_opts):
+        self.match_start = match_opts.match_start # First volume to do decoding on
         self.scores = None
     
     def match(self, t, n, tr_data):
@@ -20,14 +20,14 @@ class Matcher:
 
 class SVRMatcher(Matcher):
     """Match to templates using pretrained SVR model"""
-    def __init__(self, options):
-        super().__init__(options)
+    def __init__(self, match_opts):
+        super().__init__(match_opts)
 
-        if options.svr_path is None:
+        if match_opts.svr_path is None:
             self.log.error('SVR Model not provided. Program will exit.')
             self.mp_evt_end.set()
             sys.exit(-1)
-        self.svr_path = options.svr_path
+        self.svr_path = match_opts.svr_path
         
         try:
             with open(self.svr_path, "rb") as f:
@@ -39,35 +39,35 @@ class SVRMatcher(Matcher):
             sys.exit(-1)
 
         self.Ntemplates = len(self.SVRs.keys())
-        self.templates_labels = list(self.SVRs.keys())
-        log.info(f'List of templates to be tested: {self.templates_labels}')
+        self.template_labels = list(self.SVRs.keys())
+        log.info(f'List of templates to be tested: {self.template_labels}')
         
         self.scores = np.zeros((self.Ntemplates, 1))
 
-        self.hit_dowin = options.hit_dowin
+        self.do_win = match_opts.hit_dowin
 
-        self.hit_dowin = options.hit_dowin
-        self.hit_wl = options.hit_wl
+        self.do_win = match_opts.hit_dowin
+        self.hit_wl = match_opts.hit_wl
         
-        if self.hit_dowin:
+        if self.do_win:
             self.hit_win_weights = create_win(self.hit_wl)
 
 
     def match(self, t, n, tr_data):
-        if self.hit_dowin:
+        if self.do_win:
             tr_data = self._do_windowing(t, n, tr_data)
         
-        if t < self.dec_start_vol: # Wait until after iGLM is stable to perform matching
+        if t < self.match_start: # Wait until after iGLM is stable to perform matching
             self.scores = np.append(self.scores, np.zeros((self.Ntemplates,1)), axis=1)
         else:
-            this_t_scores = rt_svrscore_vol(np.squeeze(tr_data), self.SVRs, self.templates_labels)
+            this_t_scores = rt_svrscore_vol(np.squeeze(tr_data), self.SVRs, self.template_labels)
             self.scores   = np.append(self.scores, this_t_scores, axis=1)
         log.debug(f'[t={t},n={n}] Online - SVRs - scores.shape   {self.scores.shape}')
         
         return self.scores
     
     def _do_windowing(self, t, n, tr_data): 
-        if (t >= self.dec_start_vol):
+        if (t >= self.match_start):
             vols_in_win       = (np.arange(t-4,t)+1)[::-1]
             out_data_windowed = np.dot(tr_data[:,vols_in_win], self.hit_win_weights)
         log.debug(f'[t={t},n={n}] Online - SVRs - out_data_windowed.shape   {out_data_windowed.shape}')
