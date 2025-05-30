fc14bd194c6d7495cfa35769e6f529f65fcbd480
Marly Rubin <marlyr@protonmail.com>
2025-05-30 17:02:09 -0400

    wip: create HitDetector class, refs #23

--- rtfmri/matching/hit_detector.py
+++ rtfmri/matching/hit_detector.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from math import sqrt
 from utils.svr_methods import is_hit_rt01
 
@@ -18,10 +19,33 @@ class HitDetector:
 
         self.hit_method_func = None
         # TODO: stop using string literals
+=======
+import numpy as np
+
+from utils.svr_methods import is_hit_rt01
+
+class HitDetector:
+    """Class for deciding if a TR counts as a hit based on Matcher's scores"""
+    def __init__(self, hit_opts, mp_evt_hit):
+        self.mp_evt_hit = mp_evt_hit
+        
+
+        self.hit_method = hit_opts.hit_method
+        self.zscore_thr = hit_opts.zscore_thr
+        self.nconsec_vols  = hit_opts.nconsec_vols
+
+        # # These don't do anything yet...
+        # self.hit_domot = hit_opts.hit_domot
+        # self.hit_mot_th = hit_opts.svr_mot_th
+
+        self.hit_method_func = None
+        # TODO: stop using string literals and add checks if method doesnt exist
+>>>>>>> 328c21c (wip: create HitDetector class, refs #23)
         if self.hit_method == "method01":
             self.hit_method_func = is_hit_rt01
         else:
             raise ValueError(f"Unknown hit method: {self.hit_method}")
+<<<<<<< HEAD
 
         self.enorm_prev = None
         self.enorm_diff = None
@@ -38,6 +62,10 @@ class HitDetector:
         self.enorm_prev = enorm_current
         
     def detect(self, t, template_labels, scores, motion=None):
+=======
+    
+    def detect(self, t, template_labels, scores):
+>>>>>>> 328c21c (wip: create HitDetector class, refs #23)
         """
         Detect if a hit occured, and if so, return the name of the template.
         
@@ -52,20 +80,26 @@ class HitDetector:
         scores : np.ndarray
             The scores for each template from Matcher.
 
+<<<<<<< HEAD
         motion : list of float
             The 6 motion parameters
 
+=======
+>>>>>>> 328c21c (wip: create HitDetector class, refs #23)
         Returns
         -------
         str or None
             The label of the template that qualifies as a hit at time `t`, or None if no hit is found.
         """
+<<<<<<< HEAD
         if self.do_mot:
             self.calculate_enorm_diff(motion)
             if self.enorm_diff > self.mot_thr:
                 log.info(f'[t={t}] Motion exceeds threshold {self.mot_thr}')
                 return
 
+=======
+>>>>>>> 328c21c (wip: create HitDetector class, refs #23)
         return self.hit_method_func(
             t,
             template_labels,
@@ -73,5 +107,8 @@ class HitDetector:
             self.zscore_thr,
             self.nconsec_vols
         )
+<<<<<<< HEAD
         
+=======
+>>>>>>> 328c21c (wip: create HitDetector class, refs #23)
     
