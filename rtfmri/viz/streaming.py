67867a83195a12d0681659bb93d852a619359be0
Marly Rubin <marlyr@protonmail.com>
2025-07-01 12:56:36 -0400

    revert streaming.py to working version

--- rtfmri/viz/streaming.py
+++ rtfmri/viz/streaming.py
@@ -11,16 +11,14 @@ from holoviews.streams import Stream
 hv.extension('bokeh')
 pn.extension()
 
-def run_streamer(Nt, template_labels, match_start, mp_new_tr, mp_shm_ready, mp_evt_qa_end, mp_evt_hit):
-    streamer = Streamer(Nt, template_labels, match_start, mp_new_tr, mp_shm_ready, mp_evt_qa_end, mp_evt_hit)
+def run_streamer(Nt, template_labels, match_start, mp_new_tr, mp_shm_ready):
+    streamer = Streamer(Nt, template_labels, match_start, mp_new_tr, mp_shm_ready)
     streamer.run()
 
 class Streamer:
     """Receives scores from Matcher and starts server to stream the data live"""
-    def __init__(self, Nt, template_labels, match_start, mp_new_tr, mp_shm_ready, mp_evt_qa_end, mp_evt_hit):
+    def __init__(self, Nt, template_labels, match_start, mp_new_tr, mp_shm_ready):
         mp_shm_ready.wait()
-        self.mp_evt_qa_end = mp_evt_qa_end
-        self.mp_evt_hit = mp_evt_hit
 
         self.mp_new_tr = mp_new_tr
         self.Nt = Nt
@@ -33,53 +31,31 @@ class Streamer:
         self.shared_arr = np.ndarray((self.Ntemplates, Nt), dtype=np.float32, buffer=self.shm.buf)
 
         self.df = pd.DataFrame(np.nan, index=np.arange(self.Nt), columns=self.template_labels)
+        self.dmap = hv.DynamicMap(self.plot, streams=[Stream.define('Next')()])
 
-        self.next_stream = Stream.define('Next', t=int)(t=self.t)
-        self.dmap = hv.DynamicMap(self.plot, streams=[self.next_stream])
-
+    def update_df(self):
+        self.df.iloc[self.t] = self.shared_arr[:, self.t]
+    
     def update(self):
+        # Wait for new data, then update plot
         try:
             while self.t < self.Nt:
                 self.mp_new_tr.wait()
                 self.mp_new_tr.clear()
-
-                if self.mp_evt_hit.is_set():
-                    self.qa_onsets.append(self.t)
-                elif self.mp_evt_qa_end.is_set():
-                    self.qa_offsets.append(self.t)
-
                 self.update_df()
                 self.t += 1
-                self.next_stream.event(t=self.t)   # pass t explicitly here
+                self.dmap.event()
         finally:
             self._close_shared_memory()
-
-    def plot(self, t):
-        df_plot = self.df.iloc[:t]
-        curve = df_plot.hvplot.line(legend='top', width=1500)
-        blocks = self.draw_blocks(t)
-        
-        x_max = max(t, 50)
-        return (blocks * curve).opts(xlim=(0, x_max))
-
-    def draw_blocks(self, t):
-        height = 10
-        blocks = []
-        for onset, offset in zip(self.qa_onsets, self.qa_offsets):
-            blocks.append((onset, 0, offset, height))
-        if len(self.qa_onsets) > len(self.qa_offsets):
-            current_onset = self.qa_onsets[-1]
-            blocks.append((current_onset, 0, t, height))
-
-        color = 'cyan' if self.mp_evt_qa_end.is_set() else 'blue'
-        return hv.Rectangles(blocks).opts(alpha=0.2, color=color, line_color=None)
-
+    
+    def plot(self):
+        return self.df.iloc[:self.t].hvplot.line(legend='top', label='Match Scores', width=1500)
+    
     def run(self):
         threading.Thread(target=self.update, daemon=True).start()
         pn.serve(self.dmap, start=True, show=True)
-
+    
     def _close_shared_memory(self):
-        # TODO: move to Matcher
         self.shm.close()
         self.shm.unlink()
         
