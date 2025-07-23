c7ae087a81b98c1674b67d98194df78ac9ca8d36
Marly Rubin <marlyr@protonmail.com>
2025-07-23 13:57:15 -0400

    start testing Options methods

--- rtfmri/paths.py
+++ rtfmri/paths.py
@@ -3,6 +3,7 @@ import os.path as osp
 CODE_DIR = osp.dirname(osp.realpath(__file__))
 ROOT_DIR = osp.abspath(osp.join(CODE_DIR, '..'))
 RESOURCES_DIR = osp.join(CODE_DIR, 'resources/')
+CONFIG_DIR = osp.join(CODE_DIR, "config/")
 DATA_DIR = osp.join(ROOT_DIR, "tests/data/")
 
 SIMULATION_DIR = osp.join(ROOT_DIR, 'Simulation')
@@ -10,6 +11,3 @@ LAPTOP_DIR = osp.join(SIMULATION_DIR, 'Laptop/')
 REALTIME_DIR = osp.join(SIMULATION_DIR, 'Realtime/')
 SCANNER_DIR = osp.join(SIMULATION_DIR, 'Scanner/')
 OUTPUT_DIR = osp.join(SIMULATION_DIR, 'outputs')
-
-CAP_labels = ['VPol','DMN','SMot','Audi','ExCn','rFPa','lFPa']
-CAP_indexes = [25, 4, 18, 28, 24, 11, 21]
