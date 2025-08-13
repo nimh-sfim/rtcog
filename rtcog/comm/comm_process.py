210636564c362b7963aaeb7767b2181df3cd65c8
Marly Rubin <marlyr@protonmail.com>
2025-08-13 13:56:51 -0400

    fix streaming logic in comm_process

--- rtcog/comm/comm_process.py
+++ rtcog/comm/comm_process.py
@@ -2,6 +2,7 @@ import time
 
 from rtcog.utils.log import get_logger
 from rtcog.comm.receiver_interface import CustomReceiverInterface
+from rtcog.processor.esam_processor import ESAMProcessor
 
 log = get_logger()
 
@@ -29,15 +30,13 @@ def comm_process(opts, sync, proc_class, shared_responses=None, clock=None, time
     int
         Return code indicating success (0) or failure (1).
     """
-    log.info('2) Instantiating Processor Object...')
+    log.info('1) Initializing Processor...')
     processor = proc_class(opts, sync)
-    # TODO: move this somewhere else
-    if opts.exp_type == 'esam':
-        log.info('This an experimental run')
-        processor.start_streaming(shared_responses) # Start panel server
+    if isinstance(processor, ESAMProcessor):
+        processor.start_streaming(shared_responses) # Start streaming process
 
     # 4) Start Communications
-    log.info('3) Opening Communication Channel...')
+    log.info('2) Opening Communication Channel...')
 
     auto_save = opts.auto_save if hasattr(opts, "auto_save") else False
     receiver = CustomReceiverInterface(port=opts.tcp_port, show_data=opts.show_data, auto_save=auto_save, clock=clock, out_path=time_path)
@@ -46,14 +45,9 @@ def comm_process(opts, sync, proc_class, shared_responses=None, clock=None, time
 
     if not receiver.RTI:
         log.error('RTI is not initialized.')
-    else:
-        log.debug('RTI initialized successfully.')
-
-    if not receiver:
-        return 1
 
     # 5) set signal handlers and look for data
-    log.info('4) Setting Signal Handlers...')
+    log.info('3) Setting Signal Handlers...')
     receiver.set_signal_handlers()
 
     # 6) set receiver callback
@@ -61,21 +55,21 @@ def comm_process(opts, sync, proc_class, shared_responses=None, clock=None, time
     receiver.final_steps      = processor.end_run
 
     # 7) prepare for incoming connections
-    log.info('5) Prepare for Incoming Connections...')
+    log.info('4) Prepare for Incoming Connections...')
     if receiver.RTI.open_incoming_socket():
         return 1
     
-    # 8) Vinai's alternative
-    log.info('6) Here we go...')
+    # 8) Run experiment
+    log.info('5) Ready to go...')
     rv = receiver.process_one_run()
     
     if opts.test_latency:
         receiver.save_timing()
 
-    # TODO: refactor
-    if processor.exp_type == "esam" and not opts.no_action:
-        while processor.sync.hit.is_set():
-            log.info('waiting for QA to end ')
+    if isinstance(processor, ESAMProcessor) and not opts.no_action:
+        while sync.hit.is_set():
+            log.info('Waiting for action to end')
             time.sleep(1)
-    log.info('ready to end ')
+    log.info('Ready to end')
+
     return rv
